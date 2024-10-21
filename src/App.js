032cf81c9bf11316e675e8b9371244028ec6ddd4
«massomax» <massocode@gmail.com>
2024-10-21 17:28:49 +0300

    init

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
     return (
         <div className="App">
             <Header />
+            <Header />
             <button onClick={onToggleButton}>toggle</button>
         </div>
     );
